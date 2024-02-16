db8960ad6e22481178ecf845d1445a1cd95305ba
JD Fillmore <jd@infused.agency>
2024-02-16 13:48:48 -0500

    family health content updates

--- pages/family-health.tsx
+++ pages/family-health.tsx
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import Link from "next/link";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Accordion } from "react-bootstrap";
 
 import InnerHero from "../components/innerHero/innerHero";
 
@@ -20,35 +19,105 @@ export default function FamilyHealth() {
         <Container>
           <Row>
             <Col lg="12">
-              <h2>Uninsured Services – What You Need To Know</h2>
-              <p>
-                The Ontario Health Insurance Plan (OHIP) provides coverage for a vast majority of physician services;
-                however, there are some services that are not covered.
-              </p>
-              <p>
-                Physicians are pleased to offer these valuable services; however, the responsibility for payment lies
-                with the patient or the third party requesting the service.
-              </p>
-              <p>
-                Quality health care, medical advice and timely access are priorities that Ontario’s physicians intend to
-                honour. Speak with your physician for more information about other health care services that are not
-                covered by OHIP.
-              </p>
-              <p>
-                For more information,{" "}
-                <Link target="_blank" href="../../assets/uninsured.pdf">
-                  view a list of examples of services not covered by OHIP.
-                </Link>
-              </p>
-              <h2>Important Result Information</h2>
-              <p>
-                Due to the overwhelming number of test results received each day, we will usually only notify you if
-                follow-up is required. You may phone and inquire about your result if you have a particular concern.
-              </p>
-              <p>
-                Please allow at least 48 hours for blood work results and at least 72 hours for x-rays or other
-                investigative test results.
-              </p>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header>
+                    {" "}
+                    <h2>Test Results Turnaround Times</h2>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    Due to the overwhelming number of test results received each day, patients will be contacted to book
+                    a follow-up visit to review test results with a doctor, if necessary. If you do not receive a call
+                    and still would like to review the test results with your doctor, schedule an appointment at your
+                    convenience.{" "}
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>
+                    <h2>Uninsured Services</h2>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <p>
+                      The Ontario Health Insurance Plan (OHIP) provides coverage for a vast majority of physician
+                      services; however there are some services that are not covered. Physicians are pleased to offer
+                      these valuable services; however, the responsibility for payment lies with the patient of the
+                      third party requesting the service.
+                    </p>
+                    <p>
+                      Select medical services such as cosmetic procedures (i.e. skin tag removal) or completion of forms
+                      and medical records (i.e. sick note or insurance forms). Please note, forms may take 4 to 6 weeks
+                      for completion.
+                    </p>
+                    <p>
+                      <strong>Payment Methods:</strong>
+                    </p>
+                    <ul>
+                      <li>Credit Cards (MasterCard or Visa)</li>
+                      <li>Debit</li>
+                      <li>Cash</li>
+                    </ul>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="2">
+                  <Accordion.Header>
+                    <h2>Prescription Renewals</h2>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <p>
+                      Allow at least 1 to 2 weeks for medication renewals. For most medications, your pharmacy is able
+                      to send a request to your family doctor. New medication requests, changes or select medications
+                      require an appointment.
+                    </p>
+                    <p>Same day renewals now result in a $5 fee.</p>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="3">
+                  <Accordion.Header>
+                    <h2>Transfering Your Medical Chart</h2>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <p>
+                      A Medical Release Form must be signed to request medical chart information. Please note there is a
+                      fee of $40 for an individual or $80 for a family of two or more.
+                    </p>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="4">
+                  <Accordion.Header>
+                    <h2>Tuberculosis (TB) Testing</h2>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <p>
+                      The Mantoux tuberculin skin test can be done at our clinic with our nurses. This test is performed
+                      by injecting a small amount of non-infectious tuberculosis into the skin in the lower part of the
+                      arm. After this injection the patient must return within 48 hours to review the results and look
+                      for any reaction on the arm.
+                      <br />
+                      <br />
+                      As this testing relies on specific timing to complete check if you require a 1 Step or 2 Step test
+                      when you schedule your appointment.
+                      <br />
+                      <br />
+                      <strong>
+                        OHIP does not cover the cost of TB skin tests that are required for employment, travel or
+                        volunteer purposes.
+                      </strong>
+                    </p>
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="5">
+                  <Accordion.Header>
+                    <h2>Can I Speak to my Doctor Over the Phone?</h2>
+                  </Accordion.Header>
+                  <Accordion.Body>
+                    <p>
+                      Doctors are not able to come to the phone as they are seeing patients. Please schedule an
+                      appointment to see your doctor in the office or schedule a virtual phone appointment if
+                      applicable.
+                    </p>
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
             </Col>
           </Row>
         </Container>
